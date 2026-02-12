a59c3d7c14f0d0a0e9367267c115292c9a9664b1
serditron <sergionicolas2005221@gmail.com>
2026-02-11 19:46:01 -0500

    subi el que no era, este es el de esta clase

---
+++ src/main/java/org/example/Pedido.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Pedido {
+    private int numero;
+    private String cliente;
+
+    public Pedido() {
+    }
+
+    public Pedido(int numero, String cliente) {
+        this.numero = numero;
+        this.cliente = cliente;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "numero=" + numero +
+                ", cliente='" + cliente + '\'' +
+                '}';
+    }
+
+
+}
